c071b2294551b91503ce30bd2096a2a7f4c8d56b
Matheus Luiz M. da Silva <m.luizmsilva2@gmail.com>
2026-02-10 10:15:13 -0300

    .

---
+++ Aula 01/AulaInaugural/src/Variaveis.java
@@ -0,0 +1,12 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 20;
+        double altura = 3.5;
+        double peso = 5.5;
+        float altura2 = 3.5f;
+        char genero = 'M';
+        String nome = "john";
+
+        System.out.println("Nome: " + nome);
+    }
+}
