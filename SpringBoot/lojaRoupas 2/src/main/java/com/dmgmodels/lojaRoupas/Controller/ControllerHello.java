c8bbf9cb56ad103f89b43a1a77cdfe1a889c6ef7
Matheus Luiz M. Silva <131410397+Kei2077@users.noreply.github.com>
2026-04-30 10:49:37 -0300

    roupas

---
+++ SpringBoot/lojaRoupas 2/src/main/java/com/dmgmodels/lojaRoupas/Controller/ControllerHello.java
@@ -0,0 +1,16 @@
+package com.dmgmodels.lojaRoupas.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class ControllerHello {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello World";
+    }
+
+}
