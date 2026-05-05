16f3afcee8d61b3b97f0caecc3541118ca5a25d2
Matheus Luiz M. Silva <131410397+Kei2077@users.noreply.github.com>
2026-05-05 10:39:41 -0300

    .

---
+++ SpringBoot/lojaRoupas 2/src/main/java/com/dmgmodels/lojaRoupas/model/DadosCadastroRoupa.java
@@ -0,0 +1,11 @@
+package com.dmgmodels.lojaRoupas.model;
+
+public record DadosCadastroRoupa(
+        String nomeProduto,
+        String marca,
+        Tamanho tamanho,
+        double preco,
+        int quantidade
+
+) {
+}
---
+++ SpringBoot/lojaderoupass.sql
@@ -0,0 +1,12 @@
+create database loja;
+use loja;
+
+
+CREATE TABLE roupa (
+	id bigint auto_increment primary key,
+    nome_produto VARCHAR(255),
+    marca VARCHAR(20),
+    tamanho VARCHAR(2),
+    preco DOUBLE,
+    quantidade int
+);
